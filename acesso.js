ff62f2b76abac4d7dcc0ec731335aa6c96613c64
rodrigociroka-imile <rodrigo.ciroka@gmail.com>
2026-03-27 14:15:08 -0300

    Add files via upload

---
+++ acesso.js
@@ -0,0 +1,10 @@
+// Arquivo de controle de acesso - Senhas permitidas no sistema
+// Você pode alterar estas senhas conforme a necessidade de segurança.
+
+const SENHAS_SISTEMA = [
+    "12345",      // Senha 1
+    "admin123",   // Senha 2
+    "rodrigociroka",      // Senha 3
+    "mudar@123",  // Senha 4
+    "00000"       // Senha 5
+];
